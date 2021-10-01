4186f5420f2eb936e5f8e1131d555a1f7d2d5917
UnicornTowa <90133563+UnicornTowa@users.noreply.github.com>
2021-10-01 18:27:31 +0300

    Update theta.hpp

--- theta.hpp
+++ theta.hpp
@@ -2,11 +2,15 @@
 #include <vector>
 
 struct point {
-	short x;
-	short y;
-	operator int() const {
-		return *(int*)this;
-	}
+    short x;
+    short y;
+    point(short _x, short _y) {
+        x = _x;
+        y = _y;
+    }
+    operator int() const {
+        return *(int*)this;
+    }
 };
 
 void theta(point p_start, point p_goal, std::vector<point>& walls, std::vector<point>& path);
